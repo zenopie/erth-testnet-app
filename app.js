88ab451a77af0c0590119dc7a5527502b73e9673
Braydn Larsen <braydnl@gmail.com>
2023-09-28 15:27:40 -0700

    pending verifications

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const WEBHOOK_PORT = 3000; // Port for HTTPS
 const API_SECRET = "21b28fe5-17e1-4152-b983-d9f431da3654";
 
+let pending_verifications = [];
 
 app.use(bodyParser.json());
 // Serve static files from the 'public' directory
@@ -33,13 +34,18 @@ app.post("/api/veriff/decisions", (req, res) => {
   const payload = req.body;
 
   console.log("Received a decisions webhook");
-  console.log(
-    "Validated signature:",
-    isSignatureValid({ signature, secret, payload })
-  );
+  const isValid = isSignatureValid({ signature, secret, payload });
+  console.log("Validated signature:", isValid);
   console.log("Payload", JSON.stringify(payload, null, 4));
   res.json({ status: "success" });
-  if (payload.verification.status == "approved" && isSignatureValid({ signature, secret, payload })) {
+  let find_address = pending_verifications.indexOf(payload.verification.vendorData);
+  if (find_address != -1 && isValid){
+    pending_verifications.splice(find_address, 1);
+    console.log("spliced address ", pending_verifications);
+  } else {
+    console.log("error finding address in pending verifications");
+  }
+  if (payload.verification.status == "approved" && isValid) {
     console.log("test");
   }
 });
@@ -48,16 +54,15 @@ app.post("/api/veriff/events", (req, res) => {
   const signature = req.get("x-hmac-signature");
   const secret = API_SECRET;
   const payload = req.body;
+  const isValid = isSignatureValid({ signature, secret, payload });
 
   console.log("Received an events webhook");
-  console.log(
-    "Validated signature:",
-    isSignatureValid({ signature, secret, payload })
-  );
+  console.log("Validated signature:", isValid);
   console.log("Payload", JSON.stringify(payload, null, 4));
   res.json({ status: "success" });
-  if (payload.verification.status == "approved" && isSignatureValid({ signature, secret, payload })) {
-    console.log("test");
+  if (payload.action == "submitted" && isValid) {
+    pending_verifications.push(payload.vendorData);
+    console.log("pushed address ", pending_verifications);
   }
 });
 
